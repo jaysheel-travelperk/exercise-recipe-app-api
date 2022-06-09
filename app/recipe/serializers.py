b6122292ff37e7b7e832f01a44a59004b5c01014
jaysheel.shah <jaysheel.shah@travelperk.com>
2022-06-09 11:26:57 +0200

    Implementing Recipe

---
+++ app/recipe/serializers.py
@@ -0,0 +1,51 @@
+"""
+Serializers for recipe APIs.
+"""
+from rest_framework import serializers
+
+from core.models import (
+    Recipe,
+)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = [
+            'id',
+            'title',
+            'time_minutes',
+            'price',
+            'link',
+        ]
+        read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
+
+        return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        ingredients = validated_data.pop('ingredients', None)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
